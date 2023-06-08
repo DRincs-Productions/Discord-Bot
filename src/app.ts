d0408ba595cf7e48a0191d45a2b9a613f45fb459
Exodia <67595890+DonRP@users.noreply.github.com>
2023-06-08 17:31:23 +0000

    test

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import { addGIFReactionOnNewsPost } from "./service/ReactionService";
 import { logError, logInfo } from "./utility/Logger";
 
+let firebase_functions = require("firebase-functions");
+
+// env
 dotenv.config();
 const { DISCORD_TOKEN } = process.env;
 
@@ -24,3 +27,6 @@ if (DISCORD_TOKEN) {
 else {
     logError("Missing environment variables")
 }
+
+// * for firebase Hosting (now not used)
+exports.app = firebase_functions.https.onRequest(client);
